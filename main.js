f2b9b128d774445c75a174d88c855deec5653341
Khedr Ibrahim <108041341+KhedrIbrahim@users.noreply.github.com>
2023-01-04 06:19:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//Print the Below output
+console.log("\`I\'m In\
+\n\\\\\
+\nLove \\\\ \"\"\" \'\'\' \
+\n++ With ++ \
+\n\\\"\"\"\\\"\"\" \
+\n*/ ");
+
+/*
+
+
+`I'm In
+\\
+Love \\ """ '''
+++ With ++
+\"""\"""
+""JavaScript""``
+
+
+*/
